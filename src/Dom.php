ca88a078114df4059dc54531f59ea2e869088b27
Kerem Güneş <k-gun@mail.com>
2019-01-25 15:47:25 +0300

    Move Http\Client\Util::parseXml() as Dom::parseXml(), add Dom::$parseXmlOptions, complete create methods, optimize.

--- src/Dom.php
+++ src/Dom.php
@@ -35,18 +35,136 @@ namespace Froq\Dom;
  */
 final /* static */ class Dom
 {
-    public static function createHtmlDocument(array $data = null): object
+    /**
+     * Parse xml options.
+     * @var array
+     */
+    private static $parseXmlOptions = [
+        'preserveWhiteSpace' => false,
+        'validateOnParse' => false,
+        'strictErrorChecking' => false,
+        'throwErrors' => false
+    ];
+
+    /**
+     * Create html document.
+     * @param  array|null $data
+     * @return Froq\Dom\HtmlDocument
+     */
+    public static function createHtmlDocument(array $data = null): HtmlDocument
     {
         return new HtmlDocument($data);
     }
 
-    public static function createHmlDocument(array $data = null, string $version = null,
-        string $encoding = null): object
+    /**
+     * Create xml document.
+     * @param  array|null $data
+     * @param  string|null $version
+     * @param  string|null $encoding
+     * @return Froq\Dom\XmlDocument
+     */
+    public static function createXmlDocument(array $data = null, string $version = null,
+        string $encoding = null): XmlDocument
     {
-        return new XmlDocument($data);
+        return new XmlDocument($data, $version, $encoding);
     }
 
-    // move! /var/www/froq/froq-http-client/src/Util.php
+    /**
+     * Parse xml.
+     * @param  any  $xml
+     * @param  array $options
+     * @return any
+     * @throws Froq\Dom\DomException If $options['throwErrors'] is true.
+     */
     public static function parseXml($xml, array $options = null)
-    {}
+    {
+        $root = $xml;
+        static $error, $xmlProperties;
+
+        if (is_string($root)) {
+            $options = array_merge(self::$parseXmlOptions, $options ?? []);
+
+            $root = new \DOMDocument();
+            $root->preserveWhiteSpace = !!$options['preserveWhiteSpace'];
+            $root->validateOnParse = !!$options['validateOnParse'];
+            $root->strictErrorChecking = !!$options['strictErrorChecking'];
+
+            libxml_use_internal_errors(true);
+            $root->loadXml($xml, ((int) ($options['options'] ?? 0))
+                + LIBXML_NOERROR | LIBXML_NOWARNING | LIBXML_BIGLINES | LIBXML_COMPACT
+                    | LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+            $error = libxml_get_last_error() ?: null;
+            libxml_clear_errors();
+
+            if ($error) {
+                // god!
+                $error->file = $error->file ?: 'n/a';
+                $error->message = trim($error->message);
+                if (!!$options['throwErrors']) {
+                    throw new DomException(sprintf('Parse error (%s (level:%s code:%s column:%s'.
+                        ' file:%s line:%s))', $error->message, $error->level, $error->code,
+                        $error->column, $error->file, $error->line), $error->code);
+                }
+            }
+        }
+
+        $return = [];
+
+        // some speed up..
+        if ($xmlProperties === null) {
+            $xmlProperties = [];
+            if ($root->nodeType == XML_DOCUMENT_NODE) {
+                // add real root tag, not #document
+                $xmlProperties['@root'] = $root->firstChild->tagName ?? null;
+                $xmlProperties['@error'] = $error;
+                $xmlProperties['version'] = $root->xmlVersion;
+                $xmlProperties['encoding'] = $root->xmlEncoding;
+            }
+            $return['@xml'] = $xmlProperties;
+        }
+
+
+        if ($root->hasAttributes()) {
+            $attributes = $root->attributes;
+            foreach ($attributes as $attribute) {
+                $return['@attributes'][$attribute->name] = $attribute->value;
+            }
+        }
+
+        if ($root->hasChildNodes()) {
+            $nodes = $root->childNodes;
+            if ($nodes->length == 1) {
+                $node = $nodes->item(0);
+                if ($node->nodeType == XML_TEXT_NODE) {
+                    $return['@value'] = $node->nodeValue;
+                    return count($return) == 1 ? $return['@value'] : $return;
+                }
+            }
+
+            $groups = [];
+            foreach ($nodes as $node) {
+                $nodeName = $node->nodeName;
+                $nodeType = $node->nodeType;
+                if (!isset($return[$nodeName])) {
+                    $return[$nodeName] = self::parseXml($node, $options);
+                    // single node
+                    if ($return[$nodeName] == []) {
+                        $return[$nodeName] = null;
+                    }
+                } else {
+                    // multi nodes
+                    if (!isset($groups[$nodeName])) {
+                        $groups[$nodeName] = 1;
+                        $return[$nodeName] = [$return[$nodeName]];
+                    }
+                    $return[$nodeName][] = self::parseXml($node, $options);
+                }
+            }
+        } elseif ($root->nodeType == XML_COMMENT_NODE) {
+            $return = $root->nodeValue;
+        }
+
+        return $return;
+    }
 }
