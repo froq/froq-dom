7db4d9261a577de25d3d167a53c17031da383bf6
Kerem Güneş <qeremy@gmail.com>
2022-02-27 14:23:20 +0300

    * XmlDocument:     - add ENCODING,VERSION constants.     - optimize.

--- src/XmlDocument.php
+++ src/XmlDocument.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace froq\dom;
 
-use froq\dom\Document;
-
 /**
  * Xml Document.
  *
@@ -17,23 +15,26 @@ use froq\dom\Document;
  * @author  Kerem Güneş
  * @since   3.0
  */
-final class XmlDocument extends Document
+class XmlDocument extends Document
 {
-    /** @var string, string */
-    protected string $encoding, $version;
+    /** @const string */
+    public const ENCODING = 'utf-8', VERSION = '1.0';
+
+    /** @var string */
+    public string $encoding, $version;
 
     /**
      * Constructor.
      *
-     * @param array|null  $data
-     * @param string|null $encoding
-     * @param string|null $version
+     * @param array|null $data
+     * @param string     $encoding
+     * @param string     $version
      */
-    public function __construct(array $data = null, string $encoding = null, string $version = null)
+    public function __construct(array $data = null, string $encoding = self::ENCODING, string $version = self::VERSION)
     {
-        $this->encoding = $encoding ?: 'utf-8';
-        $this->version  = $version  ?: '1.0';
-
         parent::__construct(Document::TYPE_XML, $data);
+
+        $this->encoding = $encoding;
+        $this->version  = $version;
     }
 }
